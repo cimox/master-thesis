08482a77dd0a4aff51bae5c4e96027bfd687bb47
Matúš Cimerman <matus.cimerman@exponea.com>
2017-03-30 11:35:13 +0200

    Fix Redis msg deserialization if None

--- experiments/api/api/helpers.py
+++ experiments/api/api/helpers.py
@@ -1,10 +1,13 @@
 from api import logger
 
 
-def deserialize_redis_msg(msg):
-    if type(msg) == list:
+def deserialize_redis_msg(redis_msg):
+    if not redis_msg:
+        return 'none'
+
+    if type(redis_msg) == list:
         try:
-            return [float(e.decode('utf-8')) for e in msg]
+            return [float(e.decode('utf-8')) for e in redis_msg]
         except ValueError as e:
-            logger.exception('Deserializing redis msg: {}'.format(msg))
-    return msg.decode('utf-8')
+            logger.exception('Deserializing redis msg: {}'.format(redis_msg))
+    return redis_msg.decode('utf-8')
