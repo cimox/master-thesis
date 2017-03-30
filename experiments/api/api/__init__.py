17b02c029aa288e5bc90406c315d56ecfef4dcb3
Matúš Cimerman <matus.cimerman@exponea.com>
2017-03-30 12:36:53 +0200

    Allow CORS in API

--- experiments/api/api/__init__.py
+++ experiments/api/api/__init__.py
@@ -1,9 +1,11 @@
 import logging.config
 
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+CORS(app)
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 logger = logging.getLogger('API')
