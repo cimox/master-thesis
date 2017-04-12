f0a4225f5dee241dbcaa63f7235b289ae0a5df22
Matúš Cimerman <matus.cimerman@exponea.com>
2017-04-12 21:38:50 +0200

    Better logging for API app

--- experiments/api/api/__init__.py
+++ experiments/api/api/__init__.py
@@ -1,12 +1,13 @@
 import logging.config
 
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+app.debug = False
 CORS(app)
-FORMAT = '%(asctime)-15s %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.INFO)
+FORMAT = '%(asctime)-15s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logger = logging.getLogger('API')
 logger.info("API server has started")
