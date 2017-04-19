d0e1ad7ad977eac8ab9d29330cf289fdec289e27
Matúš Cimerman <matus.cimerman@exponea.com>
2017-04-19 08:44:50 +0200

    Move render labeling to separate function

--- experiments/vis/js/tree.js
+++ experiments/vis/js/tree.js
@@ -55,27 +55,62 @@ function updateNodeText(node) {
         });
 }
 
-function renderLabels(nodeEnter, nodeUpdate, nodeExit) {
+function renderLabels(node) {
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.parent.px + "," + d.parent.py + ")";
+        });
+
+    // Add entering nodes in the parent’s old position.
+    nodeEnter.append("svg:circle")
+        .attr("r", 6.5)
+        .style("fill", function (d) {
+            return d.leaf ? "red" : "blue";
+        });
+
     nodeEnter.append("svg:text")
-        .attr("x", function (d) {
-            return d.children || d._children ? -10 : 10;
-        })
-        .attr("dy", ".45em")
+        .attr("dy", "0.25em")
+        .attr("dx", "-0.65em")
         .attr("text-anchor", function (d) {
             return d.children || d._children ? "end" : "start";
         })
+        .style("font-family", "arial")
+        .style("color", "black")
+        .style("font-color", "black")
         .text(function (d) {
+            if (_.isEqual(d.id, "root")) return "root";
             if (d.split) {
                 return d.split.attribute + " " + d.split.operator + " " +
                     d.split.operand.toFixed(2);
             }
-            return d.className;
-        })
-        .style("fill-opacity", 1e-6);
+            return d.weights;
+        });
 
-    nodeUpdate.select("text")
-        .style("fill-opacity", 1);
+    // Transition to the proper position for the node
+    node.transition()
+        .duration(duration)
+        .attr("transform", function (d) {
+            d.px = d.x; d.py = d.y;
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+}
 
-    nodeExit.select("text")
-        .style("fill-opacity", 1e-6);
+function renderLinks(link, svg) {
+    // Add entering links in the parent’s old position.
+    link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function (d) {
+            var o = {
+                x: d.source.px,
+                y: d.source.py
+            };
+            return diagonal({source: o, target: o});
+        });
+
+    // Transition nodes and links to their new positions.
+    svg.transition()
+        .duration(duration)
+        .selectAll(".link")
+        .attr("d", diagonal);
 }
--- experiments/vis/js/utils.js
+++ experiments/vis/js/utils.js
@@ -1,8 +1,8 @@
-function hasSameProperties(obj1, obj2 ) {
+function hasSameProperties(obj1, obj2) {
     if (obj1 === undefined || obj2 === undefined) return false;
-    return Object.keys( obj1 ).every( function( property ) {
+    return Object.keys(obj1).every(function (property) {
         if (typeof obj1[property] !== 'object') {
-            return obj2.hasOwnProperty( property );
+            return obj2.hasOwnProperty(property);
         } else {
             return hasSameProperties(obj1[property], obj2[property]);
         }
@@ -27,8 +27,24 @@ function addChildren(parentNode, element) {
     }
 }
 
-function getParentNode(nodeID) {
-    return nodes[nodeIdsLookup[nodeID]];
+function getParentNode(tree, nodeID) {
+    /*
+     * BFT search tree for a given node ID
+     */
+    var queue = [];
+
+    queue.push(tree);
+    while (queue.length !== 0) {
+        var element = queue.shift();
+        if (_.isEqual(element.id, nodeID)) return element;
+
+        if (element.children !== undefined) {
+            for (var i = 0; i < element.children.length; i++) {
+                queue.push(element.children[i]);
+            }
+        }
+    }
+    return tree;
 }
 
 function updateNodeData(oldNode, newNode) {
