fb59ae3a8715ef2a1a627e7461b325659b4669be
Matúš Cimerman <matus.cimerman@exponea.com>
2017-05-06 21:50:27 +0200

    Finished my method writing, marching on results

--- experiments/moa-hfdt-extension/src/main/java/ExperimentRunner.java
+++ experiments/moa-hfdt-extension/src/main/java/ExperimentRunner.java
@@ -17,15 +17,15 @@ import java.io.PrintWriter;
 
 
 public class ExperimentRunner {
-    private final static int NUM_INSTANCES = 50000;
+    private final static int NUM_INSTANCES = 150000;
     private final static boolean IS_TESTING = true;
 
     public static void main(String[] args) {
-//        ExperimentConceptDetection exp = new ExperimentConceptDetection();
+        ExperimentConceptDetection exp = new ExperimentConceptDetection();
 //        ExperimentTelcoChurn exp = new ExperimentTelcoChurn();
 //        ExperimentRandomTree exp = new ExperimentRandomTree();
 //        ExperimentRBFND exp = new ExperimentRBFND();
-        ExperimentMyHyperPlane exp = new ExperimentMyHyperPlane();
+//        ExperimentMyHyperPlane exp = new ExperimentMyHyperPlane();
 
         exp.runExperiment(NUM_INSTANCES, IS_TESTING);
     }
